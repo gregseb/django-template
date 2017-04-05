48865920c5750c8a60abbb9c3c618ff7bf5f7ff4
Greg Sebastian <g.s.seabass@gmail.com>
2017-04-04 23:47:35 -0700

    Switched the committed settings files to templates and fixed the gitignore

--- config/settings/example.local.py
+++ config/settings/example.local.py
@@ -3,3 +3,5 @@ from .base import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='*1hvq1ebcfy2w(bj@efvv-gu@*i^%k-=73@1mrqxys1mru&gyt')
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
+
+ALLOWED_HOSTS = []
